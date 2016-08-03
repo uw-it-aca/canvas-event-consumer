9ade0bee06f9029ff36decd679a789239a80abc1
mike seibel <mikes@washington.edu>
2016-08-03 12:17:13 -0700

    only skip tsc campus true

--- events/instructor.py
+++ events/instructor.py
@@ -26,9 +26,10 @@ class InstructorEventBase(EventBase):
             course_number=course_data['CourseNumber'],
             section_id=section_data['SectionID'])
 
+        campus = section_data['CourseCampus'].lower()
         tsc = dict((t.campus.lower(),
                     t.is_on) for t in term.time_schedule_construction)
-        if tsc.get(section_data['CourseCampus'].lower(), False):
+        if campus in tsc and tsc[campus]:
             message = "Ignoring: TSC not ready: %s" % (
                 section.canvas_section_sis_id())
             if self._eventMessageType == 'uw-instructor-drop':
