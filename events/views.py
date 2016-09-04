6023ad8cc634764afa6c05502126da7f33b8df7e
mike seibel <mikes@washington.edu>
2016-09-03 21:55:37 -0700

    event rate takes a list

--- events/views.py
+++ events/views.py
@@ -13,7 +13,7 @@ class EventListView(RESTDispatch):
     """
     def GET(self, request, **kwargs):
         try:
-            event_type = request.GET.get('type', 'enrollment')
+            event_types = request.GET.get('type', 'enrollment')
             start_sample = int(floor(time() / 60))  # default to now
             end_sample = start_sample
 
@@ -33,32 +33,37 @@ class EventListView(RESTDispatch):
                             start_sample = end_sample
                             end_sample = t
 
-            events = {
-                'start': strftime("%Y-%m-%dT%H:%M:%SZ",
-                                  gmtime(start_sample * 60)),
-                'end': strftime("%Y-%m-%dT%H:%M:%SZ",
-                                gmtime(end_sample * 60)),
-                'points': [0 for i in xrange((end_sample - start_sample + 1))]
-            }
+            events = {}
+            for event_type in event_types.split(','):
+                events[event_type] = {
+                    'start': strftime("%Y-%m-%dT%H:%M:%SZ",
+                                      gmtime(start_sample * 60)),
+                    'end': strftime("%Y-%m-%dT%H:%M:%SZ",
+                                    gmtime(end_sample * 60)),
+                    'points': [0 for i in xrange(
+                        (end_sample - start_sample + 1))]
+                }
 
-            if event_type == 'enrollment':
-                event_log = EnrollmentLog.objects.filter(
-                    minute__gte=start_sample
-                )
-            elif event_type == 'instructor':
-                event_log = InstructorLog.objects.filter(
-                    minute__gte=start_sample
-                )
-            elif event_type == 'group':
-                event_log = GroupLog.objects.filter(minute__gte=start_sample)
-            else:
-                raise Exception('unknown event type %s' % event_type)
+                if event_type == 'enrollment':
+                    event_log = EnrollmentLog.objects.filter(
+                        minute__gte=start_sample
+                    )
+                elif event_type == 'instructor':
+                    event_log = InstructorLog.objects.filter(
+                        minute__gte=start_sample
+                    )
+                elif event_type == 'group':
+                    event_log = GroupLog.objects.filter(
+                        minute__gte=start_sample)
+                else:
+                    raise Exception('unknown event type %s' % event_type)
 
-            for o in event_log:
-                try:
-                    events['points'][o.minute - start_sample] = o.event_count
-                except:
-                    pass
+                for o in event_log:
+                    try:
+                        events[event_type]['points'][
+                            o.minute - start_sample] = o.event_count
+                    except:
+                        pass
 
             return self.json_response(json.dumps(events))
         except Exception as err:
